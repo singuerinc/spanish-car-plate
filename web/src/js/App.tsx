2e5edb5c9329c6a925edc30b5bad5271e9769e4d
singuerinc <nahuel.scotti@gmail.com>
2019-02-25 22:29:17 +0100

    feat(web): demo page

--- web/src/js/App.tsx
+++ web/src/js/App.tsx
@@ -1,44 +1,68 @@
-import { isValid } from "spanish-car-plate";
 import * as React from "react";
+import { useState } from "react";
+import {
+  isValid,
+  isOld,
+  isSpecial,
+  parse,
+  getCounter,
+  getNumber,
+  getProvinceName,
+  getProvinceCode,
+  getSpecialName,
+  getSpecialCode
+} from "spanish-car-plate";
 import { Check } from "./Check";
 
-class App extends React.Component {
-  state = {
-    value: ""
-  };
+export function App() {
+  const [value, setValue] = useState("");
+  const parseResult = parse(value);
 
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
+  function handleChange({ target: { value } }: { target: HTMLInputElement }) {
+    setValue(value);
   }
 
-  handleChange(event) {
-    const value: string = event.target.value;
-    this.setState({ value });
-  }
-
-  render() {
-    const { value } = this.state;
-    const isValidVal = isValid(value);
-
-    return (
-      <form className="black-80 w-100 w-50-l center">
-        <div>
-          <input
-            className="input-reset f2 f1-ns ba bw2 br3 b--silver-80 bg-white pa2 mb2 db w-100 black fw6 tc"
-            maxLength="8"
-            type="text"
-            placeholder="1234 BCD"
-            value={value}
-            onChange={this.handleChange}
-          />
-        </div>
-        <div className="flex flex-wrap justify-between mt4 mb2 pb4">
-          <Check label="isValid" check={isValidVal} />
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form className="black-80 w-100 w-50-l center">
+      <div>
+        <input
+          className="input-reset f2 f1-ns ba bw2 br3 b--silver-80 bg-white pa2 mb2 db w-100 black fw6 tc"
+          maxLength={10}
+          type="text"
+          placeholder="1234 BCD"
+          value={value}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="flex flex-wrap flex-column justify-between mt4 mb2 pb4">
+        <Check label="isValid" check={isValid(value)} />
+        <Check label="isOld" check={isOld(value)} />
+        <Check label="isSpecial" check={isSpecial(value)} />
+      </div>
+      <dl className="f6 lh-title mv2">
+        <dt className="dib b">Counter</dt>
+        <dd className="dib ml2 white">{getCounter(value) || "-"}</dd>
+      </dl>
+      <dl className="f6 lh-title mv2">
+        <dt className="dib b">Number</dt>
+        <dd className="dib ml2 white">{getNumber(value) || "-"}</dd>
+      </dl>
+      <dl className="f6 lh-title mv2">
+        <dt className="dib b">Province name</dt>
+        <dd className="dib ml2 white">{getProvinceName(value) || "-"}</dd>
+      </dl>
+      <dl className="f6 lh-title mv2">
+        <dt className="dib b">Province code</dt>
+        <dd className="dib ml2 white">{getProvinceCode(value) || "-"}</dd>
+      </dl>
+      <dl className="f6 lh-title mv2">
+        <dt className="dib b">Special name</dt>
+        <dd className="dib ml2 white">{getSpecialName(value) || "-"}</dd>
+      </dl>
+      <dl className="f6 lh-title mv2">
+        <dt className="dib b">Special code</dt>
+        <dd className="dib ml2 white">{getSpecialCode(value) || "-"}</dd>
+      </dl>
+    </form>
+  );
 }
-
-export { App };
--- web/src/js/Check.tsx
+++ web/src/js/Check.tsx
@@ -1,18 +1,22 @@
 import * as React from "react";
 import * as feather from "feather-icons";
 
-export const Check = ({ check, label }) => (
+interface IProps {
+  check: boolean;
+  label: string;
+}
+
+export const Check = ({ check, label }: IProps) => (
   <div className="flex">
     <div className="mv1">
       <div className="w-100 cf">
-        {!check && (
-          <span dangerouslySetInnerHTML={{ __html: feather.icons.x.toSvg() }} />
-        )}
-        {check && (
-          <span
-            dangerouslySetInnerHTML={{ __html: feather.icons.check.toSvg() }}
-          />
-        )}
+        <span
+          dangerouslySetInnerHTML={{
+            __html: check
+              ? feather.icons.check.toSvg()
+              : feather.icons.x.toSvg()
+          }}
+        />
       </div>
     </div>
     <div className="fw5 ml2 v-mid pv2">{label}</div>
