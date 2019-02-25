7488782789a3c146726eb467de8f3dfee2dd683d
singuerinc <nahuel.scotti@gmail.com>
2019-02-25 22:24:22 +0100

    feat: getNumber as string

--- src/getNumber.js
+++ src/getNumber.js
@@ -9,24 +9,29 @@ import { _partsNew, _partsOld, _partsSpecial } from "./_utils";
  * @returns {number}
  * @since 0.0.5
  * @example
- * getNumber("2345BCF"); // => 2345
- * getNumber("GI-1234-CS"); // => 1234
+ * getNumber("2345BCF"); // => "2345"
+ * getNumber("GI-1234-CS"); // => "1234"
  */
 function getNumber(value) {
   const str = !value ? "" : value;
+  let n;
 
   if (isOld(str) === true) {
     const [, num] = _partsOld(str);
-    return parseInt(num, 10);
+    n = num;
   } else if (isSpecial(str) === true) {
     const [, num] = _partsSpecial(str);
-    return parseInt(num, 10);
+    n = num;
   } else if (isValid(str)) {
     const [num] = _partsNew(str);
-    return parseInt(num, 10);
+    n = num;
+  } else {
+    return null;
   }
 
-  return null;
+  return parseInt(n, 10)
+    .toString()
+    .padStart(4, "0");
 }
 
 export { getNumber };
